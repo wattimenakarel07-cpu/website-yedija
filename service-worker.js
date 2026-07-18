46c747e8ed48897833a118ef71bd1b00cc5715cf
wattimenakarel07-cpu <wattimenakarel07@gmail.com>
2026-07-18 16:49:46 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,27 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
+const CACHE_NAME = "karel-coding-v1";
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(clients.claim());
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./logo-192.png",
+  "./logo-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
