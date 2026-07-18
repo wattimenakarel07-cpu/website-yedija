6607faee0b99b76355bc1b0bc8f0a3ce530aca3b
wattimenakarel07-cpu <wattimenakarel07@gmail.com>
2026-07-18 22:43:39 +0900

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4Uje8SWc7BXVW2wlFcXX6dfw2rcPVZi0",
+  authDomain: "karel-web-2.firebaseapp.com",
+  projectId: "karel-web-2",
+  storageBucket: "karel-web-2.firebasestorage.app",
+  messagingSenderId: "160144388488",
+  appId: "1:160144388488:web:723bd2e963a99d42020a3d"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+window.loginGoogle = async function () {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    alert("Selamat datang, " + result.user.displayName + "!");
+  } catch (error) {
+    alert("Login gagal: " + error.message);
+  }
+};
+
+window.logoutGoogle = async function () {
+  await signOut(auth);
+  alert("Berhasil logout.");
+};
